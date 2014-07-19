329f6acb1625a4e7e8db6044f9087fbea34cf8c1
RdMaxes <yc.huang0522@gmail.com>
2014-07-20 03:20:32 +0800

    change Lib version

--- USB_Lib/USB_App/usb_conf.h
+++ USB_Lib/USB_App/usb_conf.h
@@ -1,28 +1,21 @@
-
 /**
   ******************************************************************************
   * @file    usb_conf.h
   * @author  MCD Application Team
-  * @version V1.1.0
-  * @date    19-March-2012
-  * @brief   General low level driver configuration
+  * @version V2.0.0
+  * @date    22-July-2011
+  * @brief   general low level driver configuration
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************
   */
 
@@ -31,26 +24,7 @@
 #define __USB_CONF__H__
 
 /* Includes ------------------------------------------------------------------*/
-#if defined (USE_STM322xG_EVAL)
- #include "stm322xg_eval.h"
- #include "stm322xg_eval_lcd.h"
- #include "stm322xg_eval_ioe.h"
- #include "stm322xg_eval_sdio_sd.h"
-#elif defined(USE_STM324xG_EVAL)
- #include "stm32f4xx.h"
- #include "stm324xg_eval.h" 
- #include "stm324xg_eval_lcd.h"
- #include "stm324xg_eval_ioe.h"
- #include "stm324xg_eval_sdio_sd.h"
-#elif defined (USE_STM3210C_EVAL)
- #include "stm32f10x.h"
- #include "stm3210c_eval.h" 
- #include "stm3210c_eval_lcd.h"
- #include "stm3210c_eval_ioe.h"
- #include "stm3210c_eval_spi_sd.h"
-#else
- #error "Missing define: Evaluation board (ie. USE_STM322xG_EVAL)"
-#endif
+#include "stm32f4xx.h"
 
 
 /** @addtogroup USB_OTG_DRIVER
@@ -71,50 +45,32 @@
         configuration, you can declare the needed define in your toolchain
         compiler preprocessor.
    */
-/****************** USB OTG FS PHY CONFIGURATION *******************************
-*  The USB OTG FS Core supports one on-chip Full Speed PHY.
-*  
-*  The USE_EMBEDDED_PHY symbol is defined in the project compiler preprocessor 
-*  when FS core is used.
-*******************************************************************************/
 #ifndef USE_USB_OTG_FS
- //#define USE_USB_OTG_FS
+#define USE_USB_OTG_FS
 #endif /* USE_USB_OTG_FS */
 
-#ifdef USE_USB_OTG_FS 
- #define USB_OTG_FS_CORE
-#endif
-
-/****************** USB OTG HS PHY CONFIGURATION *******************************
-*  The USB OTG HS Core supports two PHY interfaces:
-*   (i)  An ULPI interface for the external High Speed PHY: the USB HS Core will 
-*        operate in High speed mode
-*   (ii) An on-chip Full Speed PHY: the USB HS Core will operate in Full speed mode
-*
-*  You can select the PHY to be used using one of these two defines:
-*   (i)  USE_ULPI_PHY: if the USB OTG HS Core is to be used in High speed mode 
-*   (ii) USE_EMBEDDED_PHY: if the USB OTG HS Core is to be used in Full speed mode
-*
-*  Notes: 
-*   - The USE_ULPI_PHY symbol is defined in the project compiler preprocessor as 
-*     default PHY when HS core is used.
-*   - On STM322xG-EVAL and STM324xG-EVAL boards, only configuration(i) is available.
-*     Configuration (ii) need a different hardware, for more details refer to your
-*     STM32 device datasheet.
-*******************************************************************************/
 #ifndef USE_USB_OTG_HS
  //#define USE_USB_OTG_HS
 #endif /* USE_USB_OTG_HS */
 
 #ifndef USE_ULPI_PHY
- //#define USE_ULPI_PHY
+#define USE_ULPI_PHY
 #endif /* USE_ULPI_PHY */
 
 #ifndef USE_EMBEDDED_PHY
  //#define USE_EMBEDDED_PHY
 #endif /* USE_EMBEDDED_PHY */
 
-#ifdef USE_USB_OTG_HS 
+#ifndef USE_I2C_PHY
+ //#define USE_I2C_PHY
+#endif /* USE_I2C_PHY */
+
+
+#ifdef USE_USB_OTG_FS 
+ #define USB_OTG_FS_CORE
+#endif
+
+#ifdef USE_USB_OTG_HS
  #define USB_OTG_HS_CORE
 #endif
 
@@ -150,63 +106,84 @@
 *       --> Txn should be configured with the minimum space of 16 words
 *  (v) The FIFO is used optimally when used TxFIFOs are allocated in the top 
 *       of the FIFO.Ex: use EP1 and EP2 as IN instead of EP1 and EP3 as IN ones.
-*   (vi) In HS case 12 FIFO locations should be reserved for internal DMA registers
-*        so total FIFO size should be 1012 Only instead of 1024       
+*******************************************************************************/
+
+/*******************************************************************************
+*                     FIFO Size Configuration in Host mode
+*
+*  (i) Receive data FIFO size = (Largest Packet Size / 4) + 1 or
+*                             2x (Largest Packet Size / 4) + 1,  If a
+*                             high-bandwidth channel or multiple isochronous
+*                             channels are enabled
+*
+*  (ii) For the host nonperiodic Transmit FIFO is the largest maximum packet size
+*      for all supported nonperiodic OUT channels. Typically, a space
+*      corresponding to two Largest Packet Size is recommended.
+*
+*  (iii) The minimum amount of RAM required for Host periodic Transmit FIFO is
+*        the largest maximum packet size for all supported periodic OUT channels.
+*        If there is at least one High Bandwidth Isochronous OUT endpoint,
+*        then the space must be at least two times the maximum packet size for
+*        that channel.
 *******************************************************************************/
  
 /****************** USB OTG HS CONFIGURATION **********************************/
 #ifdef USB_OTG_HS_CORE
  #define RX_FIFO_HS_SIZE                          512
- #define TX0_FIFO_HS_SIZE                          64
- #define TX1_FIFO_HS_SIZE                         372
- #define TX2_FIFO_HS_SIZE                          64
- #define TX3_FIFO_HS_SIZE                           0
- #define TX4_FIFO_HS_SIZE                           0
- #define TX5_FIFO_HS_SIZE                           0
+ #define TX0_FIFO_HS_SIZE                         512
+ #define TX1_FIFO_HS_SIZE                         512
+ #define TX2_FIFO_HS_SIZE                          0
+ #define TX3_FIFO_HS_SIZE                          0
+ #define TX4_FIFO_HS_SIZE                          0
+ #define TX5_FIFO_HS_SIZE                          0
+ #define TXH_NP_HS_FIFOSIZ                         96
+ #define TXH_P_HS_FIFOSIZ                          96
 
-// #define USB_OTG_HS_SOF_OUTPUT_ENABLED
+ //#define USB_OTG_HS_LOW_PWR_MGMT_SUPPORT
+ //#define USB_OTG_HS_SOF_OUTPUT_ENABLED
+
+ //#define USB_OTG_INTERNAL_VBUS_ENABLED
+ #define USB_OTG_EXTERNAL_VBUS_ENABLED
 
  #ifdef USE_ULPI_PHY
   #define USB_OTG_ULPI_PHY_ENABLED
  #endif
- #ifdef USE_EMBEDDED_PHY 
+ #ifdef USE_EMBEDDED_PHY
    #define USB_OTG_EMBEDDED_PHY_ENABLED
-   /* wakeup is working only when HS core is configured in FS mode */
-   #define USB_OTG_HS_LOW_PWR_MGMT_SUPPORT
  #endif
- /* #define USB_OTG_HS_INTERNAL_DMA_ENABLED */ /* Be aware that enabling DMA mode will result in data being sent only by
-                                                  multiple of 4 packet sizes. This is due to the fact that USB DMA does
-                                                  not allow sending data from non word-aligned addresses.
-                                                  For this specific application, it is advised to not enable this option
-                                                  unless required. */
+ #ifdef USE_I2C_PHY
+  #define USB_OTG_I2C_PHY_ENABLED
+ #endif
+ #define USB_OTG_HS_INTERNAL_DMA_ENABLED
  #define USB_OTG_HS_DEDICATED_EP1_ENABLED
 #endif
 
 /****************** USB OTG FS CONFIGURATION **********************************/
 #ifdef USB_OTG_FS_CORE
  #define RX_FIFO_FS_SIZE                          128
- #define TX0_FIFO_FS_SIZE                          32
+ #define TX0_FIFO_FS_SIZE                          64
  #define TX1_FIFO_FS_SIZE                         128
- #define TX2_FIFO_FS_SIZE                          32 
- #define TX3_FIFO_FS_SIZE                           0
+ #define TX2_FIFO_FS_SIZE                          0
+ #define TX3_FIFO_FS_SIZE                          0
+ #define TXH_NP_FS_FIFOSIZ                         96
+ #define TXH_P_FS_FIFOSIZ                          96
 
-// #define USB_OTG_FS_LOW_PWR_MGMT_SUPPORT
-// #define USB_OTG_FS_SOF_OUTPUT_ENABLED
+ //#define USB_OTG_FS_LOW_PWR_MGMT_SUPPORT
+ //#define USB_OTG_FS_SOF_OUTPUT_ENABLED
 #endif
-
-/****************** USB OTG MISC CONFIGURATION ********************************/
-#define VBUS_SENSING_ENABLED
 
 /****************** USB OTG MODE CONFIGURATION ********************************/
 //#define USE_HOST_MODE
 #define USE_DEVICE_MODE
 //#define USE_OTG_MODE
 
+
 #ifndef USB_OTG_FS_CORE
  #ifndef USB_OTG_HS_CORE
     #error  "USB_OTG_HS_CORE or USB_OTG_FS_CORE should be defined"
  #endif
 #endif
+
 
 #ifndef USE_DEVICE_MODE
  #ifndef USE_HOST_MODE
@@ -221,7 +198,9 @@
 #else //USE_USB_OTG_HS
  #ifndef USE_ULPI_PHY
   #ifndef USE_EMBEDDED_PHY
-     #error  "USE_ULPI_PHY or USE_EMBEDDED_PHY should be defined"
+   #ifndef USE_I2C_PHY
+     #error  "USE_ULPI_PHY or USE_EMBEDDED_PHY or USE_I2C_PHY should be defined"
+   #endif
   #endif
  #endif
 #endif
@@ -304,6 +283,5 @@
 /**
   * @}
   */ 
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
 
